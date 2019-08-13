1bc5bf502cc9a4911465bfc090eeb176066eff6f
Mingze Xiao <mxiao@box.com>
2019-08-13 10:42:18 -0700

    test content uploader

--- src/Main.js
+++ src/Main.js
@@ -2,35 +2,23 @@
 // import { addLocaleData } from 'react-intl';
 // import enLocaleData from 'react-intl/locale-data/en';
 
-import React from 'react';
-import ContentExplorer from 'box-ui-elements/es/elements/content-explorer';
-import messages from 'box-ui-elements/i18n/en-US';
-import App from './App';
-import 'box-ui-elements/dist/explorer.css';
+import React from "react";
+import ContentUploader from "box-ui-elements/es/elements/content-uploader";
+import App from "./App";
+import { IntlProvider } from "react-intl";
+// import 'box-ui-elements/dist/explorer.css';
 
 // Not needed unless working with non "en" locales
 // addLocaleData(enLocaleData);
 
-const Main = ({ token }) => (
+const token = "";
+const folderId = "";
+
+const Main = (
     <App>
-        <ContentExplorer
-            language='en-US'
-            messages={messages}
-            token={token}
-            contentPreviewProps={{
-                contentSidebarProps: {
-                    hasActivityFeed: true,
-                    hasSkills: true,
-                    hasMetadata: true,
-                    detailsSidebarProps: {
-                        hasProperties: true,
-                        hasNotices: true,
-                        hasAccessStats: true,
-                        hasVersions: true,
-                    },
-                },
-            }}
-        />
+        <IntlProvider locale="en" textComponent={React.Fragment}>
+            <ContentUploader rootFolderId={folderId} token={token} />
+        </IntlProvider>
     </App>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import 'babel-polyfill';
-import React from 'react';
-import { render } from 'react-dom';
-import Main from './Main';
-import Login from './Login';
+import "babel-polyfill";
+import React from "react";
+import { render } from "react-dom";
+import Main from "./Main";
+import Login from "./Login";
 
-const container = document.querySelector('.container');
-const grantType = 'authorization_code';
-const clientId = localStorage.getItem('clientId');
-const clientSecret = localStorage.getItem('clientSecret');
+const container = document.querySelector(".container");
+const grantType = "authorization_code";
+const clientId = localStorage.getItem("clientId");
+const clientSecret = localStorage.getItem("clientSecret");
 const { code } = queryStringToJSON();
 
 function queryStringToJSON() {
-    const pairs = location.search.slice(1).split('&');
+    const pairs = location.search.slice(1).split("&");
     const result = {};
-    pairs.forEach((pair) => {
-        pair = pair.split('=');
-        result[pair[0]] = decodeURIComponent(pair[1] || '');
+    pairs.forEach(pair => {
+        pair = pair.split("=");
+        result[pair[0]] = decodeURIComponent(pair[1] || "");
     });
     return result;
 }
@@ -28,33 +28,29 @@ function renderLogin() {
 }
 
 function renderApp() {
-    fetch('https://api.box.com/oauth2/token', {
-        method: 'POST',
-        body: `grant_type=${grantType}&code=${code}&client_id=${clientId}&client_secret=${clientSecret}`,
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-        }
-    })
-    .then((response) => {
-        if (response.status >= 200 && response.status < 300) {
-            return response.json();
-        }
-        throw new Error(response.statusText);
-    })
-    .then(({ access_token }) => {
-        render(
-            <Main token={access_token} />,
-            container
-        );
-    }).catch(() => {
-        location.href = '/';
-    });
+    // fetch('https://api.box.com/oauth2/token', {
+    //     method: 'POST',
+    //     body: `grant_type=${grantType}&code=${code}&client_id=${clientId}&client_secret=${clientSecret}`,
+    //     headers: {
+    //         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    //     }
+    // })
+    // .then((response) => {
+    //     if (response.status >= 200 && response.status < 300) {
+    //         return response.json();
+    //     }
+    //     throw new Error(response.statusText);
+    // })
+    // .then(({ access_token }) => {
+    render(<Main />, container);
+    // }).catch(() => {
+    //     location.href = '/';
+    // });
 }
+renderApp();
 
-if (code) {
-    renderApp();
-} else {
-    renderLogin();
-}
-
-
+// if (code) {
+//     renderApp();
+// } else {
+//     renderLogin();
+// }
